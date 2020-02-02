1ea793d3497a8651c8ef2473d647142d8edf3d53
Jack Matusiewicz <jack.matusiewicz@outlook.com>
2020-02-02 15:09:47 +0000

    Refactoring common code

--- Hkt/Arrow.fs
+++ Hkt/Arrow.fs
@@ -15,23 +15,25 @@ module Arrow =
     let cons : Arrow<int, int list> = Cons (Teq.refl<int>, Teq.refl<int list>)
     let rev : Arrow<int, int list> = Cons (Teq.refl<int>, Teq.refl<int list>)
 
-    let apply<'a, 'b> (ar : Arrow<'a, 'b>) : ('a * 'b) -> 'b =
+    let private constructFunc<'a, 'b, 'c, 'd>
+        (dom : Teq<'a, 'b>)
+        (range : Teq<'c, 'd>)
+        : Teq<'a * 'c -> 'c, 'b * 'd -> 'd>
+        =
+        let tupled = Teq.Cong.pair dom range
+        Teq.Cong.func tupled range
+
+    let private apply<'a, 'b> (ar : Arrow<'a, 'b>) : ('a * 'b) -> 'b =
         match ar with
         | Add (dom, range) ->
-            let tupled = Teq.Cong.pair dom range
-            let funcTeq = Teq.Cong.func tupled range
             fun (a, b) -> a + b
-            |> Teq.castFrom funcTeq
+            |> Teq.castFrom (constructFunc dom range)
         | Cons (dom, range) ->
-            let tupled = Teq.Cong.pair dom range
-            let funcTeq = Teq.Cong.func tupled range
             fun (a, b) -> a :: b
-            |> Teq.castFrom funcTeq
+            |> Teq.castFrom (constructFunc dom range)
         | Rev (dom, range) ->
-            let tupled = Teq.Cong.pair dom range
-            let funcTeq = Teq.Cong.func tupled range
             fun (a, b) -> List.append b [a]
-            |> Teq.castFrom funcTeq
+            |> Teq.castFrom (constructFunc dom range)
 
     let fold<'a,'b> (ar : Arrow<'a, 'b>) (state : 'b) (xs : 'a list) : 'b =
         let func = apply ar
